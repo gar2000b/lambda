a9dd85d4c0943f78805cbdc02e98314b575ca587
Gary Black <gar2000b@yahoo.com>
2020-05-07 18:05:17 -0400

    Enhancing with more documentation and multi-line Lambda to try to really hit home the concept.

--- src/main/java/com/onlineinteract/lambda_expressions/full/PurchaseEngine.java
+++ src/main/java/com/onlineinteract/lambda_expressions/full/PurchaseEngine.java
@@ -13,10 +13,36 @@ public class PurchaseEngine {
 
 	public void initiatePurchases() {
 		for (Customer customer : customers) {
+			/**
+			 * The way I think on it is: you are passing in a reference to a functional
+			 * interface, where that single function gets invoked.
+			 * 
+			 * The functionality/function/lambda expression itself that is passed in is
+			 * actually what is being invoked.
+			 * 
+			 * The expression itself conforms to that single functional interface - you can
+			 * see that it takes in a single argument, namely (csr), which is a Customer
+			 * type. As this functional interface method expects a return value of boolean,
+			 * we can leave it as is as we know the isPurchasedFlag() from Customer yields a
+			 * boolean and because this is a single line expression, it infers that this is
+			 * the return type.
+			 */
+			purchase(customer, csr -> csr.isPurchaseFlag());
+		}
+
+		System.out.println();
+		for (Customer customer : customers) {
+			/**
+			 * If this, however, was a multi-line lambda expression (like the following
+			 * where in this case, we are simply negating the response of our determine()
+			 * implementation), then we would need to explicitly return the boolean.
+			 */
 			purchase(customer, csr -> {
-				return csr.isPurchaseFlag();
+				boolean purchaseFlag = csr.isPurchaseFlag();
+				return !purchaseFlag;
 			});
 		}
+
 	}
 
 	private void purchase(Customer customer, Determination<Customer> determination) {
